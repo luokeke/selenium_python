e04b5f900c701dbe163e35142f602ec6ac5ecb44
luokeke <1326677806@qq.com>
2019-12-02 18:32:18 +0800

    函数

--- python_doc/Python基础语法/mooc课堂练习/drawtriangle.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf8 -*-
-#@author: 刘慧玲  2018/2/27 16:38 
--- python_doc/Python基础语法/python标准库/turtle库-海龟绘图.py
+++ python_doc/Python基础语法/python标准库/turtle库-海龟绘图.py
@@ -2,3 +2,22 @@
 # -*- coding: utf-8 -*-
 # @Time : 2019/11/27 15:19
 # @Author : liuhuiling
+
+'''turtle库的使用,python标准库，无需再次安装'''
+import turtle
+from turtle import  *
+'''turtle.setup(width,height,startx,starty)
+width,height窗体宽高
+startx,starty窗体在屏幕中位置
+turtle.setup(800,400,0,0)#窗体出现在屏幕左上方
+turtle.setup(800,400)#窗体出现在屏幕中间
+turtle.goto(100,100)#goto去到达某个位置
+turtle.fd(100)#像海龟的正前方运行
+turtle.bk(100)#像海龟的正后方运行
+turtle.circle(r,angle) #以海龟当前位置左侧某一个点为圆心曲线运行
+turtle.seth(angle) angle 为绝对度数 改变运行方向。
+turtle.left(angle) 
+turtle.right(angle) 
+RGB色彩体系 0-255  0-1小数值标识
+turtle.colormode(mode) 颜色
+'''
